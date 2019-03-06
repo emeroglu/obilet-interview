06171200c05638b7c9f86ad694807b4e992a9bad
Erhan Emre Eroğlu <emeroglu@ku.edu.tr>
2019-03-06 17:32:43 +0300

    $bcast

---
+++ obilet/Files/js/services/$bcast.js
@@ -0,0 +1,25 @@
+﻿$js.compile("$bcast", null, function ($public, $private, $protected, $self) {
+
+    $private.field.delegates = {};
+
+    $public.void.listen = function (_key, $delegate) {
+
+        if ($self.delegates[_key] == undefined)
+            $self.delegates[_key] = [];
+
+        $self.delegates[_key].push($delegate);
+
+    };
+
+    $public.void.shout = function (_key) {
+
+        if ($self.delegates[_key] == undefined)
+            return;
+
+        for (let index in $self.delegates[_key]) {
+            $self.delegates[_key][index]();
+        }
+
+    };
+
+});
