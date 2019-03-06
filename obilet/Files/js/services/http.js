f4f02be571347ac245c876e86751976d8d7f5ec3
Erhan Emre Eroğlu <emeroglu@ku.edu.tr>
2019-03-06 17:33:38 +0300

    $http

---
+++ obilet/Files/js/services/http.js
@@ -0,0 +1,48 @@
+﻿$js.compile("$http", null, function ($public, $private, $protected, $self) {
+
+    $private.field.request = null;
+
+    $public.delegate.begin = function () { $self.request = new WebRequest(); return $self; };
+
+    $private.field.method = "";
+    $public.delegate.setMethod = function (_method) { $self.method = _method; return $self; };
+
+    $private.field.url = "";
+    $public.delegate.setUrl = function (_url) { $self.url = _url; return $self; };
+
+    $private.field.data = {};
+    $public.delegate.setData = function (_data) { $self.data = _data; return $self; };
+
+    $private.void.on_success = function (_text, _json, _response) { };
+    $public.delegate.onSuccess = function ($delegate) { $self.on_success = $delegate; return $self; };
+
+    $private.void.on_error = function (_response) { };
+    $public.delegate.onError = function ($delegate) { $self.on_error = $delegate; return $self; };
+
+    $public.void.send = function () {
+
+        $self.request.set_method($self.method);
+        $self.request.set_url($self.url);
+        $self.request.set_data($self.data);
+        $self.request.onSuccess(function (_response) {
+
+            $self.on_success(_response.text, _response.json, _response);
+
+        });
+        $self.request.onError(function (_response) {
+
+            $self.on_error(_response.text, _response.json, _response);
+
+        });
+        $self.request.send();
+
+    };
+
+    $public.void.abort = function () {
+
+        if ($self.request != null)
+            $self.request.abort();
+
+    };
+
+});
